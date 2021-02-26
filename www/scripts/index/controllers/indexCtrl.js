dbf859528ac18fd96cccf0b1fe9113e2a10986a2
Nicolas Rebagliati <nicolas.rebagliati@aenima-x.com.ar>
2021-02-26 15:08:52 -0300

    update check

--- www/scripts/index/controllers/indexCtrl.js
+++ www/scripts/index/controllers/indexCtrl.js
@@ -8,16 +8,17 @@ angular.module('faradayApp')
         function($scope, $uibModal, indexFact, BASEURL) {
         	indexFact.getConf().then(function(conf) {
                 $scope.version = conf.data.ver;
-
+                // check if we are in the correct frontend
+                var version_indicator = conf.data.ver.substring(0,1);
+                if (version_indicator !== 'p'){
+                    return $location.path('/wrong_branch');
+                }
                 var osint = conf.data.osint;
                 osint.prefix = osint.prefix || "/search?query=";
                 osint.suffix = osint.suffix || "";
                 if(!osint.use_external_icon)
                     osint.icon = "images/" + osint.icon + ".png";
                 $scope.osint = osint;
-                if (conf.data.ver.substring(0,1) != 'p'){
-                    return $location.path('/wrong_branch');
-                }
         	});
 
             $scope.about = function() {
